299f2d1df8741d5d66c4c4132e32b0898ab4ffc6
medvi <tc-sport@yandex.ru>
2019-09-05 00:31:49 +0300

    Update settings.py

--- abvrating/abvrating/settings.py
+++ abvrating/abvrating/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'abvrating',
         'USER': 'root',
-        'PASSWORD': 'medvedevdenis579',
+        'PASSWORD': 'testesttest',
         'HOST': 'localhost',
         'PORT': '3306',
     }
